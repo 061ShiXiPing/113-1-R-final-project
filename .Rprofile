1045c41bba79214f1ccbe2220632d970dd1bee20
061ShiXiPing <s411385061@gm.ntpu.edu.tw>
2024-11-27 06:47:35 +0000

    update

--- .Rprofile
+++ .Rprofile
@@ -1,13 +1,1 @@
-
 library(tidyverse)
-library(ntpuR)
-
-
-file_path <- "臺北市役男徵集概況統計.csv"
-
-
-tidy_data <- read_csv(file_path, locale = locale(encoding = "UTF-8"))
-
-
-head(tidy_data)
-
--- final.R
+++ final.R
@@ -1,4 +1,3 @@
-
 library(tidyverse)
 library(ntpuR)
 
@@ -6,7 +5,7 @@ library(ntpuR)
 file_path <- "臺北市役男徵集概況統計.csv"
 
 
-tidy_data <- read_csv(file_path, locale = locale(encoding = "UTF-8"))
+tidy_data <- read_csv(file_path, locale = locale(encoding = "Big5"))
 
 
 head(tidy_data)
